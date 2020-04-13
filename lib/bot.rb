f9afa13be354f81fea85d3d13d9da3b482f14de4
Anten Skrabec <cebarks@gmail.com>
2020-04-13 07:17:34 -0600

    implements a basic "dunce" type command

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require 'discordrb'
+puts "Loading bot..."
+
+Bundler.require
 require_relative 'database'
+require_relative 'util'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['SANCBOT_TOKEN'], prefix: 's.'
 
 counter = Hash.new(0)
 db = Database.new(ENV['SANCBOT_DEBUG'])
-
-puts "This bot's invite URL is #{bot.invite_url}."
-puts 'Click on it to invite it to your server.'
 
 bot.command :ping do |event|
   event.respond 'Pong!'
@@ -20,4 +20,19 @@ bot.command :pry do |event|
   nil
 end
 
+bot.command :pshame do |event, user|
+  member = event.server.member(pick_id_from_mention(user))
+  shame_role_id = 699230129758404648
+
+  if member.role?(shame_role_id)
+    member.remove_role(shame_role_id, "Public Shaming has ended.")
+    p "Shaming has been enabled for #{member.mention}"
+  else
+    member.add_role(shame_role_id, "Public Shaming has started.")
+    p "Shaming has been disabled for #{member.mention}"
+  end
+end
+
+puts "Starting bot..."
 bot.run
+puts "Bot started!"
